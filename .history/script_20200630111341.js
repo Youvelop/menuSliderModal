3669222df6919bf17cc7c8fa0620ce3d901c07ee
Elias Molato <ec.molato@gmail.com>
2020-07-08 11:27:06 -0700

    first commit

---
+++ .history/script_20200630111341.js
@@ -0,0 +1,9 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('model');
+
+// Toggle nav
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('sho-nav')
+);
